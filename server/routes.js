0fefc0db3f3bb1d59fa2c53e1370399660d0c1d1
venkateshchavvakula198 <venkateshchavvakula198@gmail.com>
2020-05-03 18:10:10 +0530

    server side rendering

--- server/routes.js
+++ server/routes.js
@@ -7,13 +7,27 @@
 var path = require('path');
  var express =require('express')
 module.exports = function(app) {
+  app.use(express.static(path.join(__dirname, './static')));
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
-
   app.use('/auth', require('./auth'));
-  
   app.use(express.static('public'));
+  function getRoot(request, response) {
+    response.sendFile(path.resolve('./static/index.html'));
+    response.end();
+  }
+  
+  function getUndefined(request, response) {
+    response.sendFile(path.resolve('./static/index.html'));
+    response.end();
+  }
+  
+  // Note the dot at the beginning of the path
+  app.use(express.static('./static/'));
+  
+  app.get('/', getRoot);
+  app.get('/*', getUndefined);
 
 };
